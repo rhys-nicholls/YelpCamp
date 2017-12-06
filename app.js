ca3287e8910bf9e5daffc2a6790901dd8fc406a3
rhys-nicholls <Frankie7509!>
2017-12-06 15:25:21 +0000

    Added ability to add a description to a new campground. Additionally added a details page,
    - modified new.ejs form to have a description field
    - modified app.js to add description when creating a new campground
    - details page created, show.ejs. Uses ID in mongoDB to fetch info to be displayed
    - renamed campgrounds.ejs to index.ejs to follow RESTful pattern

--- app.js
+++ app.js
@@ -10,37 +10,57 @@ app.set('view engine', 'ejs');
 //Setup schema
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
+//Create model from schema
 var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Granite Hill",
+//         image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg",
+//         description: "THis is a huge granite hill, no bathrooms. Lots and lots of granite!"
+//
+//     },
+//     function (err, campground) {
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("success");
+//         }
+//     });
 
 app.get('/', function (req, res) {
     res.render('landing');
 });
 
+//INDEX - show all campgrounds
 app.get('/campgrounds', function (req, res) {
     //get all campgrounds from DB
     Campground.find({}, function (err, campgrounds) {
         if(err){
             console.log(err);
         } else {
-            res.render('campgrounds', {campgrounds: campgrounds});
+            res.render('index', {campgrounds: campgrounds});
         }
     });
 });
 
+//NEW - show form to create new campground
 app.get('/campgrounds/new', function (req, res) {
     res.render('new.ejs');
 });
 
-// POST - Add new campground
+// CREATE - Add new campground to DB
 app.post('/campgrounds', function (req, res) {
 
     //get data from form and add to array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
 
     //Create new campground and add to DB
     Campground.create(newCampground, function (err, newlyCreated) {
@@ -53,6 +73,18 @@ app.post('/campgrounds', function (req, res) {
     });
 });
 
+//SHOW - shows more info about one campground
+app.get('/campgrounds/:id', function (req, res) {
+    //find the campground with provided ID
+    Campground.findById(req.params.id, function (err, foundCampground) {
+        if(err){
+            console.log(err);
+        } else {
+            //render show template using information foudn with the ID
+            res.render('show', {campground: foundCampground});
+        }
+    });
+});
 
 //Setup listen port
 app.listen(3000, function () {
