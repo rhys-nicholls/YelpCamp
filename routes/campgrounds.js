77fb4bf9803e232ba4ebbd1600e21db91bdefcbb
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2017-12-18 14:30:51 +0000

    When adding a campground, the show page now displays the user who submitted the campground
    -used isLoggedIn middleware to ensure that a new campground can only be added when logged in
    -updated campground model to include an author object (id and username)
    -CREATE campground route changed to add the authors information to DB
    -modifiedm show page to display the username

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,18 +15,22 @@ router.get('/', function (req, res) {
 });
 
 //NEW - show form to create new campground
-router.get('/new', function (req, res) {
+router.get('/new', isLoggedIn, function (req, res) {
     res.render('campgrounds/new');
 });
 
 // CREATE - Add new campground to DB
-router.post('/', function (req, res) {
+router.post('/', isLoggedIn, function (req, res) {
 
     //get data from form and add to array
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: desc, author: author};
 
     //Create new campground and add to DB
     Campground.create(newCampground, function (err, newlyCreated) {
@@ -34,6 +38,7 @@ router.post('/', function (req, res) {
             console.log(err);
         } else {
             //redirect back to campgrounds page
+            console.log(newlyCreated);
             res.redirect('/campgrounds');
         }
     });
@@ -51,5 +56,13 @@ router.get("/:id", function(req, res){
         }
     });
 });
+
+// Middleware
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
 
 module.exports = router;
