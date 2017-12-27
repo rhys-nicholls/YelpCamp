70077393b407a9fa65151e4bdfab8825386b04ff
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2017-12-27 10:25:34 +0000

    Added ability to edit and delete a campground - added EDI, UODATE and DESTROY routes to campgrounds.js - added requirement for method-override - added an edit page for campground

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express             = require('express'),
     bodyParser          = require('body-parser'),
     mongoose            = require('mongoose'),
     passport            = require('passport'),
-    LocalStrategy       = require("passport-local");
+    LocalStrategy       = require("passport-local"),
+    methodOverride      = require('method-override'),
     Campground          = require('./models/campground');
     Comment             = require('./models/comment');
     User                = require('./models/user');
@@ -18,6 +19,7 @@ mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride('_method'));
 //seedDb();
 
 //PASSPORT config
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -57,6 +57,39 @@ router.get("/:id", function(req, res){
     });
 });
 
+//EDIT
+router.get("/:id/edit", function (req, res) {
+    Campground.findById(req.params.id, function (err, foundCampground) {
+       if(err){
+           res.redirect('/campgrounds')
+       } else {
+           res.render('campgrounds/edit', {campground: foundCampground});
+       }
+    });
+});
+
+//UPDATE
+router.put("/:id", function (req, res) {
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function (err, updatedCampground){
+        if(err){
+            res.redirect('/campgrounds');
+        } else {
+           res.redirect('/campgrounds/' + req.params.id);
+        }
+    });
+});
+
+//DESTROY
+router.delete('/:id', function (req, res) {
+    Campground.findByIdAndRemove(req.params.id, function (err) {
+        if(err){
+            res.redirect('/campgrounds');
+        } else {
+            res.redirect('/campgrounds');
+        }
+    });
+});
+
 // Middleware
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()){
